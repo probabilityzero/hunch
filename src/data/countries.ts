6a65a3375fab6109876d71d3791e98d1396303b6
Prime <primecortex@outlook.com>
2025-03-13 00:13:38 +0600

    ​

--- src/data/countries.ts
+++ src/data/countries.ts
@@ -2,141 +2,281 @@ export const countries = [
   {
     name: "United States",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a4/Flag_of_the_United_States.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5c/The_Star-Spangled_Banner.ogg", // US National Anthem URL
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a4/Flag_of_the_United_States.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5c/The_Star-Spangled_Banner.ogg", // US
     traits: ["Large", "North America", "Diverse", "Federal Republic", "English", "50 States"],
   },
   {
     name: "Canada",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/c/cf/Flag_of_Canada.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/2/29/O_Canada.ogg", // Canadian National Anthem URL
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/c/cf/Flag_of_Canada.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/2/29/O_Canada.ogg", // Canadian
     traits: ["Vast", "North America", "Bilingual", "Constitutional Monarchy", "French/English", "10 Provinces"],
   },
   {
     name: "Brazil",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/0/05/Flag_of_Brazil.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/e/ea/Hino_Nacional_Brasileiro.ogg", // Brazilian National Anthem URL
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/0/05/Flag_of_Brazil.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/e/ea/Hino_Nacional_Brasileiro.ogg", // Brazilian
     traits: ["Large", "South America", "Diverse", "Federal Republic", "Portuguese", "Amazon Rainforest"],
   },
   {
     name: "United Kingdom",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a4/Flag_of_the_United_Kingdom.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a0/UK_National_Anthem.ogg", // UK National Anthem URL
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a4/Flag_of_the_United_Kingdom.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a0/UK_National_Anthem.ogg", // UK
     traits: ["Island Nation", "Europe", "Constitutional Monarchy", "English", "4 Countries", "Historic"],
   },
   {
     name: "Japan",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/9/9e/Flag_of_Japan.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Kimi_ga_Yo.ogg", // Japanese National Anthem URL
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/9/9e/Flag_of_Japan.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Kimi_ga_Yo.ogg", // Japanese
     traits: ["Island Nation", "East Asia", "Technologically Advanced", "Constitutional Monarchy", "Japanese", "Unique Culture"],
   },
   {
     name: "Germany",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/b/ba/Flag_of_Germany.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/f/f1/Deutschlandlied.ogg", // German National Anthem URL
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/b/ba/Flag_of_Germany.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/f/f1/Deutschlandlied.ogg", // German
     traits: ["Central Europe", "Industrialized", "German-speaking", "Federal Republic", "Historic", "Engineering"],
   },
   {
     name: "France",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/c/c3/Flag_of_France.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5f/La_Marseillaise.ogg", // French National Anthem URL
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/c/c3/Flag_of_France.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5f/La_Marseillaise.ogg", // French
     traits: ["Cultural", "Western Europe", "Romantic", "Republic", "French", "Wine", "Artistic"],
   },
   {
     name: "Australia",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/b/b9/Flag_of_Australia.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/c/c6/Advance_Australia_Fair.ogg", // Australian National Anthem URL
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/b/b9/Flag_of_Australia.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/c/c6/Advance_Australia_Fair.ogg", // Australian
     traits: ["Island", "Oceania", "English-speaking", "Commonwealth", "Sporty", "Natural Wonders", "Friendly"],
   },
   {
     name: "China",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/0/0f/Flag_of_China.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/9/97/The_March_of_the_Volunteers.ogg", // Chinese National Anthem URL
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/0/0f/Flag_of_China.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/9/97/The_March_of_the_Volunteers.ogg", // Chinese
     traits: ["Most Populous", "East Asia", "Communist", "Mandarin-speaking", "Historic", "Industrialized", "Ancient Civilizations"],
   },
   {
     name: "India",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/4/41/Flag_of_India.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/8/86/Jana_Gana_Mana.ogg", // Indian National Anthem URL
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/4/41/Flag_of_India.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/8/86/Jana_Gana_Mana.ogg", // Indian
     traits: ["South Asia", "Cultural Diversity", "Democratic", "Hindu-majority", "English-speaking", "Historic", "Large Population"],
   },
   {
     name: "South Africa",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/aa/Flag_of_South_Africa.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/7/7b/Republic_of_South_Africa_National_Anthem.ogg", // South African National Anthem URL
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/aa/Flag_of_South_Africa.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/7/7b/Republic_of_South_Africa_National_Anthem.ogg", // South African
     traits: ["Southern Africa", "Diverse", "Multilingual", "Republic", "English-speaking", "Wildlife", "Rainbow Nation"],
   },
   {
     name: "Italy",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/0/03/Flag_of_Italy.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/0/02/Italian_National_Anthem.ogg", // Italian National Anthem URL
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/0/03/Flag_of_Italy.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/0/02/Italian_National_Anthem.ogg", // Italian
     traits: ["Southern Europe", "Romantic", "Historic", "Cultural", "Italian-speaking", "Cuisine", "Fashion"],
   },
   {
     name: "Mexico",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/f/fc/Flag_of_Mexico.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/1/1d/Himno_Nacional_Mexicano.ogg", // Mexican National Anthem URL
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/f/fc/Flag_of_Mexico.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/1/1d/Himno_Nacional_Mexicano.ogg", // Mexican
     traits: ["North America", "Spanish-speaking", "Cultural", "Republic", "Cuisine", "Football", "Colorful"],
   },
   {
     name: "Argentina",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/1/1a/Flag_of_Argentina.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Himno_Nacional_Argentino.ogg", // Argentine National Anthem URL
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/1/1a/Flag_of_Argentina.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Himno_Nacional_Argentino.ogg", // Argentine
     traits: ["South America", "Spanish-speaking", "Cultural", "Football", "Wine", "Vibrant", "Tango"],
   },
   {
     name: "Russia",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/f/f3/Flag_of_Russia.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/7/7a/State_Anthem_of_the_Russian_Federation.ogg", // Russian National Anthem URL
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/f/f3/Flag_of_Russia.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/7/7a/State_Anthem_of_the_Russian_Federation.ogg", // Russian
     traits: ["Largest Country", "Eastern Europe", "Cultural", "Slavic", "Russian-speaking", "Historic", "Cold"],
   },
   {
     name: "Egypt",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Flag_of_Egypt.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5e/Arab_Republic_of_Egypt_National_Anthem.ogg", // Egyptian National Anthem URL
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Flag_of_Egypt.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5e/Arab_Republic_of_Egypt_National_Anthem.ogg", // Egyptian
     traits: ["North Africa", "Historic", "Arab-speaking", "Desert", "Pharaohs", "Pyramids", "Islamic"],
   },
   {
     name: "Turkey",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Flag_of_Turkey.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/c/c4/Turkish_National_Anthem.ogg", // Turkish National Anthem URL
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/b/b4/Flag_of_Turkey.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/c/c4/Turkish_National_Anthem.ogg", // Turkish
     traits: ["Transcontinental", "Historic", "Turkish-speaking", "Republic", "Cultural", "Cuisine", "Straddling Two Continents"],
   },
   {
     name: "South Korea",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/0/0f/Flag_of_South_Korea.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/4/44/Aegukga.ogg", // South Korean National Anthem URL
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/0/0f/Flag_of_South_Korea.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/4/44/Aegukga.ogg", // South Korean
     traits: ["East Asia", "Technologically Advanced", "Democratic", "Korean-speaking", "Cultural", "Efficient", "Pop Culture"],
   },
   {
     name: "Nigeria",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/7/79/Flag_of_Nigeria.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/1/16/Nigeria_National_Anthem.ogg", // Nigerian National Anthem URL
-    traits: ["West Africa", "Most Populous in Africa", "English-speaking", "Democratic", "Cultural Diversity", "Oil-rich"],
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/7/7f/Flag_of_Nigeria.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Nigerian_National_Anthem.ogg", // Nigerian
+    traits: ["West Africa", "English-speaking", "Diverse", "Federal Republic", "Oil-rich", "Cultural", "Nollywood"],
+  },
+  {
+    name: "Nigeria",
+    category: "countries",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/7/7f/Flag_of_Nigeria.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/4/4b/Nigerian_National_Anthem.ogg",
+    traits: ["West Africa", "English-speaking", "Democratic", "Large Population", "Oil-rich", "Cultural Diversity"],
+  },
+  {
+    name: "Spain",
+    category: "countries",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Flag_of_Spain.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/1/1d/Spanish_National_Anthem.ogg",
+    traits: ["Southern Europe", "Spanish-speaking", "Monarchy", "Cuisine", "Football", "Tourism"],
+  },
+  {
+    name: "Saudi Arabia",
+    category: "countries",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/8/83/Flag_of_Saudi_Arabia.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/e/e1/Saudi_National_Anthem.ogg",
+    traits: ["Middle East", "Arabic-speaking", "Monarchy", "Oil-rich", "Conservative", "Islamic"],
+  },
+  {
+    name: "Indonesia",
+    category: "countries",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/9/9f/Flag_of_Indonesia.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Indonesia_National_Anthem.ogg",
+    traits: ["Southeast Asia", "Island Nation", "Indonesian-speaking", "Democratic", "Diverse", "Tropical"],
   },
   {
     name: "Thailand",
     category: "countries",
-    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Flag_of_Thailand.svg", // Flag URL
-    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Song_of_Thailand.ogg", // Thai National Anthem URL
-    traits: ["Southeast Asia", "Tropical", "Buddhist", "Monarchy", "Cuisine", "Culture", "Tourist Destination"],
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a9/Flag_of_Thailand.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/0/07/Thailand_National_Anthem.ogg",
+    traits: ["Southeast Asia", "Monarchy", "Thai-speaking", "Buddhist", "Tourism", "Cuisine"],
+  },
+  {
+    name: "Egypt",
+    category: "countries",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/f/fe/Flag_of_Egypt.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5e/Arab_Republic_of_Egypt_National_Anthem.ogg",
+    traits: ["North Africa", "Historic", "Arab-speaking", "Desert", "Pharaohs", "Pyramids", "Islamic"],
+  },
+  {
+    name: "Pakistan",
+    category: "countries",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/3/32/Flag_of_Pakistan.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/d/d3/Pakistani_National_Anthem.ogg",
+    traits: ["South Asia", "Urdu-speaking", "Islamic Republic", "Cultural", "Democratic", "Mountainous"],
+  },
+  {
+    name: "Vietnam",
+    category: "countries",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/2/21/Flag_of_Vietnam.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/f/f7/Vietnamese_National_Anthem.ogg",
+    traits: ["Southeast Asia", "Communist", "Vietnamese-speaking", "Historical", "Rice", "Mountains"],
+  },
+  {
+    name: "Sweden",
+    category: "countries",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/4/4c/Flag_of_Sweden.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/2/28/Swedish_National_Anthem.ogg",
+    traits: ["Northern Europe", "Scandinavian", "Monarchy", "Swedish-speaking", "High Standard of Living", "Welfare State"],
+  },
+  {
+    name: "Norway",
+    category: "countries",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Norway.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/4/43/Norwegian_National_Anthem.ogg",
+    traits: ["Northern Europe", "Scandinavian", "Monarchy", "Norwegian-speaking", "Natural Beauty", "Oil-rich"],
+  },
+  {
+    name: "Finland",
+    category: "countries",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/b/bc/Flag_of_Finland.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/e/e6/Finnish_National_Anthem.ogg",
+    traits: ["Northern Europe", "Scandinavian", "Democratic", "Finnish-speaking", "High Quality of Life", "Education"],
+  },
+  {
+    name: "Switzerland",
+    category: "countries",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/f/f7/Flag_of_Switzerland.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/7/7e/Swiss_National_Anthem.ogg",
+    traits: ["Central Europe", "Neutral", "Multilingual", "Democratic", "Swiss Franc", "Alps", "Banking"],
+  },
+  {
+    name: "South Korea",
+    category: "countries",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/0/0f/Flag_of_South_Korea.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/4/44/Aegukga.ogg",
+    traits: ["East Asia", "Technologically Advanced", "Democratic", "Korean-speaking", "Cultural", "Efficient", "Pop Culture"],
+  },
+  {
+    name: "Malaysia",
+    category: "countries",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/6/66/Flag_of_Malaysia.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5b/Malaysian_National_Anthem.ogg",
+    traits: ["Southeast Asia", "Malaysian-speaking", "Constitutional Monarchy", "Diverse", "Tourism", "Tropical"],
+  },
+  {
+    name: "Singapore",
+    category: "countries",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/4/48/Flag_of_Singapore.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/4/4a/Singapore_National_Anthem.ogg",
+    traits: ["Southeast Asia", "English-speaking", "Highly Urbanized", "Republic", "Cultural Fusion", "Economic Hub"],
+  },
+  {
+    name: "Colombia",
+    category: "countries",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/2/21/Flag_of_Colombia.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/6/63/Colombian_National_Anthem.ogg",
+    traits: ["South America", "Spanish-speaking", "Republic", "Coffee", "Biodiversity", "Cultural"],
+  },
+  {
+    name: "Chile",
+    category: "countries",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/7/78/Flag_of_Chile.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/a/a5/Chilean_National_Anthem.ogg",
+    traits: ["South America", "Spanish-speaking", "Democratic", "Mountainous", "Wine", "Seafood"],
+  },
+  {
+    name: "Peru",
+    category: "countries",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/c/cf/Flag_of_Peru.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/2/22/Peruvian_National_Anthem.ogg",
+    traits: ["South America", "Spanish-speaking", "Cultural", "Andes Mountains", "Historic", "Incan Heritage"],
+  },
+  {
+    name: "Romania",
+      category: "countries",
+      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/7/73/Flag_of_Romania.svg",
+      soundUrl: "https://upload.wikimedia.org/wikipedia/commons/8/85/Romanian_National_Anthem.ogg",
+      traits: ["Eastern Europe", "Romanian-speaking", "Democratic", "Cultural", "Carpathian Mountains", "Historic", "Diverse Landscapes"],
+  },
+  {
+      name: "Moldova",
+      category: "countries",
+      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/2/27/Flag_of_Moldova.svg",
+      soundUrl: "https://upload.wikimedia.org/wikipedia/commons/5/56/Moldovan_National_Anthem.ogg",
+      traits: ["Eastern Europe", "Romanian-speaking", "Republic", "Agricultural", "Wine-producing", "Cultural Heritage", "Former Soviet Union"],
+  },
+  {
+    name: "Poland",
+    category: "countries",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/1/12/Flag_of_Poland.svg",
+    soundUrl: "https://upload.wikimedia.org/wikipedia/commons/4/46/Polish_National_Anthem.ogg",
+    traits: ["Central Europe", "Slavic", "Polish-speaking", "Democratic", "Historic", "Cultural"],
   },
 ];
